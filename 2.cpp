efad9032f6c95f29727744e1756ee04f015f065e
AjiePerdanaP <32450431+AjiePerdanaP@users.noreply.github.com>
2017-11-07 05:45:51 +0000

    Input dan Output nilai

--- 2.cpp
+++ 2.cpp
@@ -8,5 +8,19 @@ int main (int argc, char** argv){
 	cout << "Pilihan Hitungan\n";
 	cout << "1.Persegi Panjang\n";
 	cout << "2.Segitiga\n";cin >>pilihan;
+	
+	switch(pilihan){
+		case 1:
+			cout <<"Masukan Panjang =";cin>>panjang;
+			cout <<"Masukan Lebar =";cin>>lebar;
+			luas =panjang * lebar;
+			cout<<"Luas Persegi Panjang="<<luas<<"\n";
+			break;
+			case 2:
+			cout<<"Masukan Panjang Alas Segitiga=";cin>>alas;
+			cout<<"Masukan Tinggi Segitiga=";cin>>tinggi;
+			luas=0.5*alas*tinggi;
+			cout<<"Luas Segitiga ="<<luas<<"\n";
+	}
 }
 
