0e0025951269322af35dd439d5c9c2477f560cb9
AjiePerdanaP <32450431+AjiePerdanaP@users.noreply.github.com>
2017-11-07 05:38:44 +0000

    Output Nilai

--- 2.cpp
+++ 2.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
-int main (){
+int main (int argc, char** argv){
+	int panjang, lebar, luas, pilihan, alas, tinggi;
 	
+	cout << "Pilihan Hitungan\n";
+	cout << "1.Persegi Panjang\n";
+	cout << "2.Segitiga\n";cin >>pilihan;
 }
+
